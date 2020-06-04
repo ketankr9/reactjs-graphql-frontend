d278c24ce7a8f7f4daf5f86d021c35ce3744f077
ketankr9 <utsav.krishnan.cse15@iibhu.ac.in>
2020-06-04 11:58:45 +0530

    working as max:curren game

--- src/App.js
+++ src/App.js
@@ -28,8 +28,20 @@ const setLocalStorage = (newLocalState) => {
     localStorage.setItem("gameXYZstate", JSON.stringify(newLocalState));
 };
 
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.0/8 are considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+  );
 
-const GRAPHQL_SERVER = window.location.hostname === "localhost" ? "http://localhost:8080/graphql" : "https://superna.serveo.net/graphql";
+let GRAPHQL_SERVER = "https://impleo.serveo.net/graphql";
+if(isLocalhost)
+        GRAPHQL_SERVER = "http://localhost:8080/graphql";
+
 console.log("GRAPHQL_SERVER: ", GRAPHQL_SERVER);
 const client = new ApolloClient({ uri: GRAPHQL_SERVER });
 
